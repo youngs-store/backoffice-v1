5088324c8f0ce95924d912f24a78ea5a094425a3
leo <youngdong.kim@kakao.com>
2022-04-19 14:33:13 +0900

    fix:Header.js modify

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
                 <button id="btnCloseToMenu" className="btn--menu" type="button"><span className="material-icons">menu</span></button>
                 <h1 className="logo"><a href="/"><img src="https://youcandoo.yanadoocdn.com/v3/web/assets/images/web_logo_blue.svg" alt="로고" /></a></h1>
             </div>
-            <button id="btnAddToItem" className="btn--add" type="button" onClick={Aside.foldingAside()}><span className="material-icons add-icons">add</span><span className="add-title">항목 추가 등록하기</span></button>
+            <button id="btnAddToItem" className="btn--add" type="button" ><span className="material-icons add-icons">add</span><span className="add-title">항목 추가 등록하기</span></button>
             <nav className="nav--list">
                 <ul>
                     <li><a href="{()=>false}"><span className="material-icons">turned_in</span><span>항목1</span></a></li>
